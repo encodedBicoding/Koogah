76bce205b7274520da7c53c4daf1aa50facc2377
encodedBicoding <dominicisioma000@gmail.com>
2019-11-09 12:28:44 +0100

    extra changes to fix bugs on package.json

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,6 +2,7 @@
 import BearerStrategy from 'passport-http-bearer';
 import jwt from '../api/v1/helpers/jwt';
 import Couriers from '../../database/models/courier';
+import Customers from '../../database/models/customers';
 
 class Auth {
   static bearerStrategy() {
@@ -10,7 +11,7 @@ class Auth {
 
       if (!payload) return done(null, false);
 
-      const is_found_user = await Couriers.findOne({
+      let is_found_user = await Couriers.findOne({
         where: {
           email: payload.email,
         },
@@ -18,7 +19,16 @@ class Auth {
 
       if (!is_found_user) {
         // check main users table
-        return done(null, false);
+        is_found_user = await Customers.findOne({
+          where: {
+            email: payload.email,
+          },
+        });
+        if (!is_found_user) {
+          return done(null, false);
+        }
+
+        return done(null, is_found_user);
       }
       return done(null, is_found_user);
     });

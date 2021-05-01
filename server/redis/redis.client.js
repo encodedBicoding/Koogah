9fbb2657ea132400169900aab0d67d849a7bc36a
encodedBicoding <dominicisioma000@gmail.com>
2021-05-01 12:46:38 +0100

    fix up redis

--- server/redis/redis.client.js
+++ server/redis/redis.client.js
@@ -1,14 +1,17 @@
 import redis from 'redis';
 import { config } from 'dotenv';
 import log from 'fancy-log';
+import url from 'url';
 
 config();
 // eslint-disable-next-line import/no-mutable-exports
 let client;
 const isProduction = process.env.NODE_ENV === 'production';
-
+var rtg;
 if (isProduction) {
-  client = redis.createClient(process.env.REDIS_URL);
+  rtg = url.parse(process.env.REDIS_URL);
+  client = redis.createClient(rtg.port, rtg.hostname);
+  client.auth(rtg.auth.split(':')[1]);
 }
 if (process.env.NODE_ENV === 'test') {
   if (process.env.REDIS_URL_TEST) {

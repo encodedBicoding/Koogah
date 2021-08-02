da3fa949e392fd611b836cca9ddc5b9e818ba884
encodedBicoding <dominicisioma000@gmail.com>
2021-08-02 22:17:40 +0100

    update sms and transfer charges

--- server/api/v1/controllers/payment.controller.js
+++ server/api/v1/controllers/payment.controller.js
@@ -345,7 +345,9 @@ class Payment {
             error: 'Oops, seems this dispatcher doesn\'t exists anymore...',
           });
         }
-        const fees = Number(is_package_valid.delivery_price) * 0.30;
+        const sms_charge = 50;
+        const transfer_charge = 10;
+        const fees = (Number(is_package_valid.delivery_price) * 0.30) + sms_charge + transfer_charge;
         const total_amount_payable = Number(is_package_valid.delivery_price) - fees;
         const dispatcher_new_balance = Number(dispatcher.virtual_balance) + total_amount_payable;
   
